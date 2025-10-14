37f1791cde63f5739905b4f9caf4b47a00255caf
Robel Gebremeskel <rofikre@yahoo.com>
2025-10-14 16:38:52 -0400

    added Private folders

---
+++ routing-demo/src/app/_lib/page.tsx
@@ -0,0 +1,8 @@
+export default function PrivateRoute() {
+    return (
+        <main>
+            <h1>Private Route</h1>
+            <p>This is a private route that requires authentication.</p>
+        </main>
+    );
+}
