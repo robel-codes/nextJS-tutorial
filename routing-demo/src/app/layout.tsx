a7ec660b18a2b8f4cc7b710b4f67bd67503e297e
Robel Gebremeskel <rofikre@yahoo.com>
2025-10-14 16:59:16 -0400

    added layout with header and footer

--- routing-demo/src/app/layout.tsx
+++ routing-demo/src/app/layout.tsx
@@ -6,11 +6,29 @@ export const metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <header
+          style={{
+            backgroundColor: "lightblue",
+            padding: "1rem",
+          }}
+        >
+          <p>Header</p>
+        </header>
+        {children}
+        <footer
+          style={{
+            backgroundColor: "ghostwhite",
+            padding: "1rem",
+          }}
+        >
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
