76f342abe9f18c7e362b43b42c85ab1c88edff1b
Robel Gebremeskel <rofikre@yahoo.com>
2025-10-15 16:19:27 -0400

    added title metadata

--- routing-demo/src/app/not-found.tsx
+++ routing-demo/src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
     return (
         <main>
             <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
+            <p>Sorry, couldnot find requested resource.</p>
         </main>
     );
 }
