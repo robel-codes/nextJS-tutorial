01a7f8b1f05d464bc79160cf65c59d3bf94f6a2a
Robel Gebremeskel <rofikre@yahoo.com>
2025-10-15 17:19:13 -0400

    added link component and active links

--- routing-demo/src/app/page.tsx
+++ routing-demo/src/app/page.tsx
@@ -1,8 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Product</Link>
     </main>
   );
 }
